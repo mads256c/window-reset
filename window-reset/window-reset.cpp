a4e35e424c8f89bcc2e701d97f02fa465b350865
Mads Mogensen <mads256c@elev.aatg.dk>
2019-09-25 22:52:06 +0200

    Remove unneeded vector

--- window-reset/window-reset.cpp
+++ window-reset/window-reset.cpp
@@ -2,16 +2,20 @@
 //
 
 #include <iostream>
-#include <vector>
-
 #include <Windows.h>
 
 
-BOOL CALLBACK EnumWindowsProc(HWND hwnd, const LPARAM windows)
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM)
 {
 	if (IsWindowVisible(hwnd))
 	{
-		reinterpret_cast<std::vector<HWND>*>(windows)->push_back(hwnd);
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+
+		if (rect.right <= 0 || rect.left < -10)
+		{
+			SetWindowPos(hwnd, nullptr, 1920 + rect.left, rect.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
+		}
 	}
 	
 	return TRUE;
@@ -19,20 +23,7 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, const LPARAM windows)
 
 int main()
 {
-	std::vector<HWND> windows;
-	windows.reserve(512);
-	EnumWindows(EnumWindowsProc, reinterpret_cast<LPARAM>(&windows));
-
-	for (const auto window : windows)
-	{
-		RECT rect;
-		GetWindowRect(window, &rect);
-
-		if (rect.right <= 0 || rect.left < -10)
-		{
-			SetWindowPos(window, nullptr, 1920 + rect.left, rect.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
-		}
-	}
+	EnumWindows(EnumWindowsProc, 0);
 
 	return EXIT_SUCCESS;
 }
