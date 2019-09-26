6437a3c2c442c9eb3f091c5d3bfea07ef938accd
Mads Mogensen <mads256c@elev.aatg.dk>
2019-09-26 13:22:20 +0200

    Handles way more monitors and configurations

--- window-reset/window-reset.c
+++ window-reset/window-reset.c
@@ -9,10 +9,15 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM unused)
 		RECT rect;
 		GetWindowRect(hwnd, &rect);
 
-		if (rect.right <= 0 || rect.left < -10)
-		{
-			SetWindowPos(hwnd, NULL, 1920 + rect.left, rect.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
-		}
+		HMONITOR hMonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTOPRIMARY);
+
+		MONITORINFO monitor;
+		monitor.cbSize = sizeof(monitor);
+		GetMonitorInfoA(hMonitor, &monitor);
+
+		if (monitor.dwFlags & MONITORINFOF_PRIMARY) return TRUE;
+
+		SetWindowPos(hwnd, NULL, -monitor.rcMonitor.left + rect.left, -monitor.rcMonitor.top + rect.top, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_NOACTIVATE);
 	}
 
 	return TRUE;
@@ -20,36 +25,41 @@ BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM unused)
 
 _Releases_lock_(hMutex)
 int CALLBACK WinMain(
-    HINSTANCE   hInstance,
-    HINSTANCE   hPrevInstance,
-    LPSTR       lpCmdLine,
-    int         nCmdShow
-    )
+	HINSTANCE   hInstance,
+	HINSTANCE   hPrevInstance,
+	LPSTR       lpCmdLine,
+	int         nCmdShow
+)
 {
 	UNREFERENCED_PARAMETER(hInstance);
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(nCmdShow);
-	
+
 	HANDLE hMutex = OpenMutex(
-      MUTEX_ALL_ACCESS, 0, L"window-reset");
+		MUTEX_ALL_ACCESS, 0, L"window-reset");
 
 	if (hMutex)
 	{
 		MessageBoxA(NULL, "Another instance of this program is running.", "Window Reset", MB_OK);
 		return EXIT_FAILURE;
 	}
-	
+
 	hMutex = CreateMutex(0, 0, L"window-reset");
 
+	if (!hMutex)
+	{
+		return EXIT_FAILURE;
+	}
 
 	if (!RegisterHotKey(NULL, 1, MOD_CONTROL | MOD_ALT | MOD_NOREPEAT, 0x4D))
 	{
+		ReleaseMutex(hMutex);
 		MessageBoxA(NULL, "Failed to create hotkey.", "Window Reset", MB_OK);
 		return EXIT_FAILURE;
 	}
 
-	MSG msg = { 0 };
+	MSG msg = { 0, 0, 0, 0, 0, {0, 0} };
 	while (GetMessage(&msg, NULL, 0, 0) != 0)
 	{
 		if (msg.message == WM_HOTKEY)
